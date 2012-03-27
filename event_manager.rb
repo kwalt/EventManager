13f933b57df00ca777a496d2f03416f96b20190e
Kurt Walters <walters.kurt@gmail.com>
2012-03-26 22:23:58 -0400

    Everything done except Iteraions 4 and 7

--- event_manager.rb
+++ event_manager.rb
@@ -1,9 +1,11 @@
 # Dependencies
 require "csv"
+require "sunlight"
 
 # Class Definition
 class EventManager
 	INVALID_ZIPCODE = "00000"
+  Sunlight::Base.api_key = "e179a6973728c4dd3fb1204283aaccb5"
   def initialize(filename)
     puts "EventManager Initialized."
     @file = CSV.open(filename, {:headers => true, :header_converters => :symbol})
@@ -82,12 +84,87 @@ class EventManager
     end
   end
 
+  def rep_lookup
+  	20.times do
+  		line = @file.readline
+
+  		representative = "unknown"
+  		#API Lookup goes here
+  		legislators = Sunlight::Legislator.all_in_zipcode(clean_zipcode(line[:zipcode]))
+			# legislators.each do |leg|
+			# 	puts leg.firstname
+			#end
+			names = legislators.collect do |leg|
+			  #first_name = leg.firstname
+			  #first_initial = first_name[0]
+			  first_initial = leg.first_name[0] #why not do it this way?
+			  last_name = leg.lastname
+			  party_initial = leg.party
+			  first_initial + ". " + last_name + "(" + party_initial + ")"
+			end
+
+			puts "#{line[:last_name]}, #{line[:first_name]}, #{line[:zipcode]}, #{names.join(", ")}"
+  	end
+  end
+
+  def create_form_letters
+    letter = File.open("./event_manager/form_letter.html", "r").read
+    20.times do
+      line = @file.readline
+
+      custom_letter = letter.gsub("#first_name","#{line[:first_name]}")
+			custom_letter = custom_letter.gsub("#last_name","#{line[:last_name]}")
+			custom_letter = custom_letter.gsub("#street","#{line[:street]}")
+			custom_letter = custom_letter.gsub("#city","#{line[:city]}")
+			custom_letter = custom_letter.gsub("#state","#{line[:state]}")
+			custom_letter = custom_letter.gsub("#zipcode","#{line[:zipcode]}")
+			
+			filename = "./event_manager/output/thanks_#{line[:last_name]}_#{line[:first_name]}.html"
+			output = File.new(filename, "w")
+			output.write(custom_letter)
+		end
+  end
+
+  def rank_times
+    hours = Array.new(24){0}
+    @file.each do |line|
+      regdate = line[:regdate]
+      hour = regdate.split(" ")[1].split(":")[0]
+			hours[hour.to_i] = hours[hour.to_i] + 1
+
+    end
+    hours.each_with_index{|counter,hour| puts "#{hour}\t#{counter}"}
+  end
+
+  def day_stats
+  	days = Array.new(7){0}
+  	@file.each do |line|
+  		numerals = line[:regdate].split(" ")[0]
+  		date = Date.strptime(numerals, "%m/%d/%Y")
+  		day_of_week = date.wday
+  		days[day_of_week.to_i] = days[day_of_week.to_i] + 1
+  	end
+  	days.each_with_index{|counter,day_of_week| puts "#{day_of_week}\t#{counter}"}
+  end
+
+  def state_stats
+    state_data = {}
+    @file.each do |line|
+
+    end
+  end
+
+
 end
 
 
 # Script
-manager = EventManager.new("./event_manager/event_attendees.csv")
+manager = EventManager.new("./event_manager/event_attendees_clean.csv")
 #manager.print_names
 #manager.print_numbers
 #manager.print_zipcodes
-manager.output_data("./event_manager/event_attendees_clean.csv")
+#manager.output_data("./event_manager/event_attendees_clean.csv")
+#manager.rep_lookup
+#manager.create_form_letters
+#manager.rank_times
+manager.day_stats
